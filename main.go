a4cccdd2f4eeda5308eceba4bf7eea719e347196
李彦辉 <943514589@qq.com>
2022-02-19 17:58:13 +0800

    feat: 引入 gin，自己实现打开 chrome 和 关闭 chrome

--- main.go
+++ main.go
@@ -1,25 +1,93 @@
 package main
 
 import (
-	"github.com/zserge/lorca"
+	"github.com/gin-gonic/gin"
 	"os"
+	"os/exec"
 	"os/signal"
+	"runtime"
 	"syscall"
 )
 
-func main() {
-	var ui lorca.UI
-	// 打开一个 chrome 窗口
-	ui, _ = lorca.New("https://www.zhihu.com", "", 800, 600, "--disable-sync", "--disable-translate")
-	// 创建一个频道连接系统信号
-	chSignal := make(chan os.Signal, 1)
-	// 监听系统信号，如果是 interrupt 或者 termination
-	signal.Notify(chSignal, syscall.SIGINT, syscall.SIGTERM)
-	// 等待 ui 结束，或者 chSignal 收到信号
-	select {
-	case <-ui.Done():
-	case <-chSignal:
+var ChromeExecutable = LocateChrome
+
+// LocateChrome returns a path to the Chrome binary, or an empty string if
+// Chrome installation is not found.
+func LocateChrome() string {
+
+	// If env variable "LORCACHROME" specified and it exists
+	if path, ok := os.LookupEnv("LORCACHROME"); ok {
+		if _, err := os.Stat(path); err == nil {
+			return path
+		}
 	}
-	// 等待完毕，关闭 UI，结束主线程
-	ui.Close()
+
+	var paths []string
+	switch runtime.GOOS {
+	case "darwin":
+		paths = []string{
+			"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+			"/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary",
+			"/Applications/Chromium.app/Contents/MacOS/Chromium",
+			"/usr/bin/google-chrome-stable",
+			"/usr/bin/google-chrome",
+			"/usr/bin/chromium",
+			"/usr/bin/chromium-browser",
+		}
+	case "windows":
+		paths = []string{
+			os.Getenv("LocalAppData") + "/Google/Chrome/Application/chrome.exe",
+			os.Getenv("ProgramFiles") + "/Google/Chrome/Application/chrome.exe",
+			os.Getenv("ProgramFiles(x86)") + "/Google/Chrome/Application/chrome.exe",
+			os.Getenv("LocalAppData") + "/Chromium/Application/chrome.exe",
+			os.Getenv("ProgramFiles") + "/Chromium/Application/chrome.exe",
+			os.Getenv("ProgramFiles(x86)") + "/Chromium/Application/chrome.exe",
+			os.Getenv("ProgramFiles(x86)") + "/Microsoft/Edge/Application/msedge.exe",
+		}
+	default:
+		paths = []string{
+			"/usr/bin/google-chrome-stable",
+			"/usr/bin/google-chrome",
+			"/usr/bin/chromium",
+			"/usr/bin/chromium-browser",
+			"/snap/bin/chromium",
+		}
+	}
+
+	for _, path := range paths {
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			continue
+		}
+		return path
+	}
+	return ""
+}
+
+func ginfunc() {
+	gin.SetMode(gin.DebugMode)
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func startChrome(url string) *exec.Cmd {
+	var arg = "--app=" + url
+	chrome := exec.Command(ChromeExecutable(), arg)
+	chrome.Run()
+	return chrome
+}
+
+func main() {
+	go ginfunc()
+	chrome := startChrome("http://127.0.0.1:8080")
+
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, os.Kill, syscall.SIGUSR1, syscall.SIGUSR2)
+	<-c
+	chrome.Process.Kill()
 }
