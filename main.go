b359c775c144d7cbf8d3dc11ac1e647d5bafff10
李彦辉 <943514589@qq.com>
2022-03-01 22:30:01 +0800

    feat: 使用 webview 的方法实现

--- main.go
+++ main.go
@@ -3,75 +3,18 @@ package main
 import (
 	"embed"
 	"github.com/gin-gonic/gin"
+	"github.com/webview/webview"
 	"io/fs"
 	"log"
 	"net/http"
 	"os"
-	"os/exec"
-	"os/signal"
-	"runtime"
 	"strings"
-	"syscall"
 )
-
-var ChromeExecutable = LocateChrome
-
-// LocateChrome returns a path to the Chrome binary, or an empty string if
-// Chrome installation is not found.
-func LocateChrome() string {
-
-	// If env variable "LORCACHROME" specified and it exists
-	if path, ok := os.LookupEnv("LORCACHROME"); ok {
-		if _, err := os.Stat(path); err == nil {
-			return path
-		}
-	}
-
-	var paths []string
-	switch runtime.GOOS {
-	case "darwin":
-		paths = []string{
-			"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-			"/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary",
-			"/Applications/Chromium.app/Contents/MacOS/Chromium",
-			"/usr/bin/google-chrome-stable",
-			"/usr/bin/google-chrome",
-			"/usr/bin/chromium",
-			"/usr/bin/chromium-browser",
-		}
-	case "windows":
-		paths = []string{
-			os.Getenv("LocalAppData") + "/Google/Chrome/Application/chrome.exe",
-			os.Getenv("ProgramFiles") + "/Google/Chrome/Application/chrome.exe",
-			os.Getenv("ProgramFiles(x86)") + "/Google/Chrome/Application/chrome.exe",
-			os.Getenv("LocalAppData") + "/Chromium/Application/chrome.exe",
-			os.Getenv("ProgramFiles") + "/Chromium/Application/chrome.exe",
-			os.Getenv("ProgramFiles(x86)") + "/Chromium/Application/chrome.exe",
-			os.Getenv("ProgramFiles(x86)") + "/Microsoft/Edge/Application/msedge.exe",
-		}
-	default:
-		paths = []string{
-			"/usr/bin/google-chrome-stable",
-			"/usr/bin/google-chrome",
-			"/usr/bin/chromium",
-			"/usr/bin/chromium-browser",
-			"/snap/bin/chromium",
-		}
-	}
-
-	for _, path := range paths {
-		if _, err := os.Stat(path); os.IsNotExist(err) {
-			continue
-		}
-		return path
-	}
-	return ""
-}
 
 //go:embed frontend/dist/*
 var FS embed.FS
 
-func ginfunc() {
+func ginFunc() {
 	router := gin.Default()
 
 	staticFiles, _ := fs.Sub(FS, "frontend/dist")
@@ -102,20 +45,12 @@ func ginfunc() {
 	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
-func startChrome(url string) *exec.Cmd {
-	var arg = "--app=" + url
-	chrome := exec.Command(ChromeExecutable(), arg)
-	chrome.Run()
-	return chrome
-}
-
 func main() {
-	go ginfunc()
-	chrome := startChrome("http://127.0.0.1:8080/")
-
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt, os.Kill, syscall.SIGUSR1, syscall.SIGUSR2)
-	<-c
-	chrome.Process.Kill()
-	os.Exit(1)
+	go ginFunc()
+	w := webview.New(true)
+	defer w.Destroy()
+	w.SetTitle("GoEasy")
+	w.SetSize(800, 600, webview.HintNone)
+	w.Navigate("http://127.0.0.1:8080")
+	w.Run()
 }
