1aff0572ddf4cf226086eb37140649dd14b31b09
李彦辉 <943514589@qq.com>
2022-03-05 16:31:41 +0800

    feat: 实现 uploads 接口

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func ginFunc() {
 	router := gin.Default()
 
 	router.GET("/api/v1/addresses", AddressesController)
+	router.GET("/uploads/:path", UploadsController)
 	router.POST("/api/v1/texts", TextsController)
 
 	staticFiles, _ := fs.Sub(FS, "frontend/dist")
@@ -109,4 +110,33 @@ func AddressesController(c *gin.Context) {
 		}
 	}
 	c.JSON(http.StatusOK, gin.H{"addresses": result})
+}
+
+func GetUploadsDir() (uploads string) {
+	// 获取执行文件的路径
+	exePath, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 获取执行文件的目录路径
+	dirPath := filepath.Dir(exePath)
+	// 获取 uploads 目录路径
+	uploads = filepath.Join(dirPath, "uploads")
+	return
+}
+
+
+func UploadsController(c *gin.Context) {
+	// 将网络路径 :path 变成本地路径
+	// 获取本地文件，写到 HTTP 响应里
+	if path := c.Param("path"); path != "" {
+		target := filepath.Join(GetUploadsDir(), path)
+		c.Header("Content-Description", "File Transfer")
+		c.Header("Content-Transfer-Encoding", "binary")
+		c.Header("Content-Disposition", "attachment; filename="+path)
+		c.Header("Content-Type", "application/octet-stream")
+		c.File(target)
+	} else {
+		c.Status(http.StatusNotFound)
+	}
 }
