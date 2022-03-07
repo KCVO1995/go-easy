a850a9b574ee27127d5cc8bf69ada298a4fee753
李彦辉 <943514589@qq.com>
2022-03-07 22:26:18 +0800

    feat: 将 server 和 openWebview 方法模块化

--- go.mod
+++ go.mod
@@ -3,24 +3,25 @@ module github.com/KCVO1995/go-easy
 go 1.17
 
 require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/google/uuid v1.3.0
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+	github.com/webview/webview v0.0.0-20220228175528-9bda02f8085c
+)
+
+require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.7.7 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/uuid v1.3.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
 	github.com/ugorji/go/codec v1.2.6 // indirect
-	github.com/webview/webview v0.0.0-20220228175528-9bda02f8085c // indirect
-	github.com/zserge/lorca v0.1.10 // indirect
 	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
-	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
---
+++ server/server.go
@@ -0,0 +1,189 @@
+package server
+
+import (
+	"embed"
+	"io/fs"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/skip2/go-qrcode"
+)
+
+//go:embed frontend/dist/*
+var FS embed.FS
+
+var Port = "27149"
+
+func Run () {
+	router := gin.Default()
+
+	router.GET("/api/v1/addresses", AddressesController)
+	router.GET("/uploads/:path", UploadsController)
+	router.GET("/api/v1/qrcodes", QrcodesController)
+	router.POST("/api/v1/texts", TextsController)
+	router.POST("/api/v1/files", FilesController)
+
+	staticFiles, _ := fs.Sub(FS, "frontend/dist")
+	router.StaticFS("/static", http.FS(staticFiles))
+
+	router.NoRoute(func(context *gin.Context) {
+		path := context.Request.URL.Path
+		if strings.HasPrefix(path, "/static") {
+			file, err := staticFiles.Open("index.html")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer file.Close()
+			stat, err := file.Stat()
+			if err != nil {
+				log.Fatal(err)
+			}
+			context.DataFromReader(http.StatusOK, stat.Size(), "text/html", file, nil)
+		} else {
+			context.Status(http.StatusNotFound)
+		}
+	})
+	router.Run(":" + Port)
+}
+
+func TextsController(c *gin.Context) {
+	// 1. 获取到文本
+	// 2. 获取安装路径
+	// 3. 生产一个文件，使用 hash 命名
+	// 4. 返回下载链接
+	var json struct {
+		Row string `json:"raw"`
+	}
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	} else {
+		// 获取执行文件的路径
+		exePath, err := os.Executable()
+		if err != nil {
+			log.Fatal(err)
+		}
+		// 获取执行文件的目录路径
+		dirPath := filepath.Dir(exePath)
+		// 常见 uploads 文件夹
+		uploads := filepath.Join(dirPath, "uploads")
+		errMkdir := os.MkdirAll(uploads, os.ModePerm)
+		if errMkdir != nil {
+			log.Fatal(err)
+		}
+
+		// 拼接文件的绝对路径，不包含 exe 目录
+		filename := uuid.New().String()
+		fullPath := filepath.Join("uploads", filename+".txt")
+		errWriteFile := ioutil.WriteFile(filepath.Join(dirPath, fullPath), []byte(json.Row), 0644)
+		if errWriteFile != nil {
+			log.Fatal(err)
+		}
+
+		// 返回文件路径
+		c.JSON(http.StatusOK, gin.H{"url": "/" + fullPath})
+	}
+
+}
+
+func AddressesController(c *gin.Context) {
+	// 获取电脑端所有 ip 地址
+	// 通过 json 返回给前端
+	addrs, _ := net.InterfaceAddrs()
+	var result []string
+	for _, address := range addrs {
+		if ipNet, ok := address.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+			if ipNet.IP.To4() != nil {
+				result = append(result, ipNet.IP.String())
+			}
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{"addresses": result})
+}
+
+func GetUploadsDir() (uploads string) {
+	// 获取执行文件的路径
+	exePath, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 获取执行文件的目录路径
+	dirPath := filepath.Dir(exePath)
+	// 获取 uploads 目录路径
+	uploads = filepath.Join(dirPath, "uploads")
+	return
+}
+
+
+func UploadsController(c *gin.Context) {
+	// 将网络路径 :path 变成本地路径
+	// 获取本地文件，写到 HTTP 响应里
+	if path := c.Param("path"); path != "" {
+		target := filepath.Join(GetUploadsDir(), path)
+		c.Header("Content-Description", "File Transfer")
+		c.Header("Content-Transfer-Encoding", "binary")
+		c.Header("Content-Disposition", "attachment; filename="+path)
+		c.Header("Content-Type", "application/octet-stream")
+		c.File(target)
+	} else {
+		c.Status(http.StatusNotFound)
+	}
+}
+
+func QrcodesController (c * gin.Context) {
+	// 获取文本内容
+	// 将文本转为图片
+	// 将图片写入 HTTP 响应
+	if content := c.Query("content"); content != "" {
+		qrcode, err := qrcode.Encode(content, qrcode.Medium, 256)
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.Data(http.StatusOK, "image/png", qrcode)
+	} else {
+		c.Status(http.StatusBadRequest)
+	}
+}
+
+func FilesController (c * gin.Context) {
+	// 获取 go 执行文件所在目录
+	// 在该目录常见 uploasd 目录
+	// 将上传文件保存为另一个文件
+	// 返回后者的下载路径
+	// file = c.form
+	file, err := c.FormFile("raw")
+	if err != nil {
+			log.Fatal(err)
+	}
+	// 获取执行文件的路径
+	exePath, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 获取执行文件的目录路径
+	dirPath := filepath.Dir(exePath)
+	// 常见 uploads 文件夹
+	uploads := filepath.Join(dirPath, "uploads")
+	errMkdir := os.MkdirAll(uploads, os.ModePerm)
+	if errMkdir != nil {
+		log.Fatal(err)
+	}
+
+	// 拼接文件的绝对路径，不包含 exe 目录
+	filename := uuid.New().String()
+	fullPath := filepath.Join("uploads",  filename + filepath.Ext(file.Filename))
+	errWriteFile := c.SaveUploadedFile(file, filepath.Join(dirPath, fullPath))
+	if errWriteFile != nil {
+		log.Fatal(err)
+	}
+
+	// 返回文件路径
+	c.JSON(http.StatusOK, gin.H{"url": "/" + fullPath})	
+}
