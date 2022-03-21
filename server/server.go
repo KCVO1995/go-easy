071736636a79fd49b334deeaca139a3c51f6470d
李彦辉 <943514589@qq.com>
2022-03-21 22:44:53 +0800

    feat: 加入 ws 通讯，完成手机端上传后通知 PC

--- server/server.go
+++ server/server.go
@@ -7,8 +7,9 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/KCVO1995/go-easy/server/controller"
 	"github.com/gin-gonic/gin"
+	"github.com/KCVO1995/go-easy/server/ws"
+	"github.com/KCVO1995/go-easy/server/controller"
 )
 
 //go:embed frontend/dist/*
@@ -17,6 +18,8 @@ var FS embed.FS
 var Port = "27149"
 
 func Run () {
+	hub := ws.NewHub()
+	go hub.Run()
 	router := gin.Default()
 
 	router.GET("/api/v1/addresses", controller.AddressesController)
@@ -24,6 +27,9 @@ func Run () {
 	router.GET("/api/v1/qrcodes", controller.QrcodesController)
 	router.POST("/api/v1/texts", controller.TextsController)
 	router.POST("/api/v1/files", controller.FilesController)
+	router.GET("/ws", func(c *gin.Context) {
+		ws.HttpController(c, hub)
+	})
 
 	staticFiles, _ := fs.Sub(FS, "frontend/dist")
 	router.StaticFS("/static", http.FS(staticFiles))
